9d8cca55b42360446a4f6153915c50dd138913b5
Jeffrey Clark <dude@zaplabs.com>
2016-04-05 17:55:10 -0500

    cleanup disable uxme for now

--- builders.py
+++ builders.py
@@ -2,35 +2,16 @@ import textwrap
 import itertools
 
 from buildbot.config import BuilderConfig
-from buildbot.process.properties import Interpolate
-from buildbot.steps.source.git import Git
-from buildbot.steps.shell import Compile, Test, ShellCommand
-from buildbot.steps.slave import RemoveDirectory, MakeDirectory
-from buildbot.steps.transfer import FileDownload, FileUpload
 
 from cfgbb.common import repos, environment
-from cfgbb.slaves import slaves, get_slaves, names, lock
+from cfgbb.slaves import lock
 from cfgbb import factories
 
 builders = []
 
-class FileUploadWithUrls(FileUpload):
-	renderables = ["auxUrls"]
-
-	def __init__(self, auxUrls=None, **kwargs):
-		self.auxUrls = auxUrls
-		FileUpload.__init__(self, **kwargs)
-		self.addFactoryArguments(auxUrls=auxUrls)
-
-	def start(self):
-		if self.auxUrls is not None:
-			for url in self.auxUrls:
-				self.addURL(os.path.basename(url), url)
-		FileUpload.start(self)
-
 for rname, rcfg in repos.items():
 	for bn in rcfg['builders']:
-		f = getattr(factories, rname)(rcfg)
+		f = getattr(factories, rname)(rcfg, bn)
 		builders.append(
 			BuilderConfig(
 				name=rname+'-'+bn,
--- factories.py
+++ factories.py
@@ -1,73 +1,118 @@
-from buildbot.process.factory import BuildFactory
-from buildbot.steps.shell import Compile, ShellCommand
-from buildbot.steps.source.git import Git
+from buildbot.plugins import steps, util
 
-def attractmode(cfg):
-	f = BuildFactory()
-	f.addStep(
-		Git(
+#class FileUploadWithUrls(FileUpload):
+#	renderables = ["auxUrls"]
+#
+#	def __init__(self, auxUrls=None, **kwargs):
+#		self.auxUrls = auxUrls
+#		FileUpload.__init__(self, **kwargs)
+#		self.addFactoryArguments(auxUrls=auxUrls)
+#
+#	def start(self):
+#		if self.auxUrls is not None:
+#			for url in self.auxUrls:
+#				self.addURL(os.path.basename(url), url)
+#		FileUpload.start(self)
+
+def attractmode(cfg, buildname):
+	bf = util.BuildFactory()
+	bf.addStep(
+		steps.Git(
 		repourl=cfg['url'],
 			branch=cfg['branch'],
 			mode='full',
-			method='clean',
 			logEnviron=False
 		)
 	)
 	if 'gitversion' in cfg:
-		f.addStep(cfg['gitversion'])
-	f.addStep(
-		ShellCommand(
+		bf.addStep(cfg['gitversion'])
+	bf.addStep(
+		steps.Compile(
+			command=['bash', '-c', 'util/win/create-pkg.sh'],
+			haltOnFailure=True
+		)
+	)
+	if buildname == "osx":
+		bf.addStep(
+			steps.MultipleFileUpload(
+				slavesrcs=[
+					util.Interpolate("attract-%(prop:gitversion)s.dmg"),
+				],
+				masterdest="~/sites/com.zaplabs/build/public/project/attractmode/osx",
+				url="/project/attractmode/osx",
+				haltOnFailure=False, flunkOnFailure=False, mode=0644
+			)
+		)
+	elif buildname == "windows":
+		bf.addStep(
+			steps.MultipleFileUpload(
+				slavesrcs=[
+					util.Interpolate('attract-%(prop:gitversion)s-win32.zip'),
+					util.Interpolate('attract-%(prop:gitversion)s-win64.zip'),
+				],
+				masterdest="~/sites/com.zaplabs/build/public/project/attractmode/windows",
+				url="/project/attractmode/windows",
+				haltOnFailure=False, flunkOnFailure=False, mode=0644
+			)
+		)
+	return bf
+
+def uxme(cfg, buildname):
+	bf = util.BuildFactory()
+	bf.addStep(
+		steps.Git(
+			repourl=cfg['url'],
+			branch=cfg['branch'],
+			mode='full',
+			logEnviron=False
+		)
+	)
+	if 'gitversion' in cfg:
+		bf.addStep(cfg['gitversion'])
+	bf.addStep(
+		steps.ShellCommand(
 			name="create package",
 			command=['bash', '-c', 'util/win/create-pkg.sh'],
 			description='make and package',
 			haltOnFailure=True
 		)
 	)
-	return f
+	return bf
 
-def uxme(cfg):
-	f = BuildFactory()
-	f.addStep(
-		Git(
-		repourl=cfg['url'],
+def mame(cfg, buildname):
+	bf = util.BuildFactory()
+	bf.addStep(
+		steps.Git(
+			repourl=cfg['url'],
 			branch=cfg['branch'],
 			mode='full',
-			method='clean',
 			logEnviron=False
 		)
 	)
 	if 'gitversion' in cfg:
-		f.addStep(cfg['gitversion'])
-	f.addStep(
-		ShellCommand(
-			name="create package",
-			command=['bash', '-c', 'util/win/create-pkg.sh'],
-			description='make and package',
+		bf.addStep(cfg['gitversion'])
+	bf.addStep(
+		steps.ShellCommand(
+			name="fetch support",
+			command=["git clone --depth=1 --branch mame https://github.com/zaplabs/buildsupport.git .buildsupport"],
+			description="download latest build support tool",
 			haltOnFailure=True
 		)
 	)
-	return f
-
-def mame(cfg):
-	f = BuildFactory()
-	f.addStep(
-		Git(
-		repourl=cfg['url'],
-			branch=cfg['branch'],
-			mode='full',
-			method='clean',
-			logEnviron=False
-		)
-	)
-	if 'gitversion' in cfg:
-		f.addStep(cfg['gitversion'])
-	f.addStep(
-		ShellCommand(
-			name="create package",
-			command=['bash', '-c', 'util/win/create-pkg.sh'],
-			description='make and package',
+	bf.addStep(
+		steps.Compile(
+			command=['bash', '-c', '.buildsupport/build.sh'],
 			haltOnFailure=True
 		)
 	)
-	return f
-
+	bf.addStep(
+		steps.MultipleFileUpload(
+			slavesrcs=[
+				util.Interpolate("mame-mingw-gcc-x32-%(prop:gitversion)s.exe")
+			],
+			masterdest="~/sites/com.zaplabs/build/public/project/mame",
+			url="/project/mame",
+			haltOnFailure=False, flunkOnFailure=False, mode=0644
+		)
+	)
+	return bf
